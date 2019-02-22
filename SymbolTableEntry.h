ba666f41f558aae94f049985687c794e749512a5
BostonMessbarger <47875613+BostonMessbarger@users.noreply.github.com>
2019-02-21 20:30:34 -0600

    Add program code and some testing files

---
+++ SymbolTableEntry.h
@@ -0,0 +1,57 @@
+#ifndef SYMBOL_TABLE_ENTRY_H
+#define SYMBOL_TABLE_ENTRY_H
+
+#include <string>
+using namespace std;
+
+#define UNDEFINED  			-1   // Type codes
+#define INT				1
+#define STR				2
+#define INT_OR_STR			3
+#define BOOL				4
+#define INT_OR_BOOL			5
+#define STR_OR_BOOL			6
+#define INT_OR_STR_OR_BOOL		7
+
+   
+#define NOT_APPLICABLE 	-1
+
+typedef struct 
+{ 
+  int type;       // one of the above type codes
+  int int_value;
+  char* str_value;
+  bool bool_value;
+} TYPE_INFO;
+
+class SYMBOL_TABLE_ENTRY 
+{
+private:
+  // Member variables
+  string name;
+  TYPE_INFO typeInfo;
+
+public:
+  // Constructors
+  SYMBOL_TABLE_ENTRY( ) 
+  { 
+    name = ""; 
+    typeInfo.type = UNDEFINED; 
+  }
+
+  SYMBOL_TABLE_ENTRY(const string theName, 
+                     const TYPE_INFO theType) 
+  {
+    name = theName;
+    typeInfo.type = theType.type;
+	typeInfo.int_value = theType.int_value;
+	typeInfo.str_value = theType.str_value;
+	typeInfo.bool_value = theType.bool_value;
+  }
+
+  // Accessors
+  string getName() const { return name; }
+  TYPE_INFO getTypeInfo() const { return typeInfo; }
+};
+
+#endif  // SYMBOL_TABLE_ENTRY_H
